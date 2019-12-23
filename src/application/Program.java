c6b1baf3acd4244b24d04d8d0be8608586913e82
jmslapa <lapeta96@gmail.com>
2019-12-23 17:26:26 -0300

    Atualizadas classes DaoFactory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department d = new Department(1, "Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller s = new Seller(1, "John", "john@gmail.com", new Date(), 3000.0, d);
+		Seller s = sellerDao.findById(3);
 		
 		System.out.println(s);
 	}
