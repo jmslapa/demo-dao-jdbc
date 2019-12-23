0fc839d2a470f5009cca119ce9fb1eab5fe7665d
jmslapa <lapeta96@gmail.com>
2019-12-23 18:47:22 -0300

    Implementado método insert em SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,6 +28,11 @@ public class Program {
 		list = sellerDao.findAll();
 		for(Seller s2 : list)
 			System.out.println(s2);
+		
+		System.out.println("\n                 TEST 4 : INSERTION (seller)");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted. New id: " + newSeller.getId());
 	}
 
 }
