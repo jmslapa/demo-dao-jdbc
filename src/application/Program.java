561d70fbfce89db85cd2c6585da197d898955efe
jmslapa <lapeta96@gmail.com>
2019-12-23 18:10:00 -0300

    Atualizados SellerDao e SellerDaoJDBC com método para recuperar um seller tendo um department como argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,10 +13,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("                 TEST 1 : FIND BY ID (SELLER)");
+		System.out.println("                 TEST 1 : FIND BY Id (seller)");
 		Seller s = sellerDao.findById(3);
-		
 		System.out.println(s);
+		
+		Department department = new Department(2, "Electrônics");
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		System.out.println("\n                 TEST 2 : FIND BY Department (seller)");
+		for(Seller s2 : list)
+			System.out.println(s2);
 	}
 
 }
